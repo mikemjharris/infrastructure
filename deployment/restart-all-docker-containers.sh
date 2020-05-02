0129438bcca66b91d22235980dc43f3a7618c14a
Mike Harris <hello@mikemjharris.com>
2020-05-02 06:44:12 +0000

    use rm

--- deployment/restart-all-docker-containers.sh
+++ deployment/restart-all-docker-containers.sh
@@ -1,19 +1,15 @@
 docker kill snowman-multi
-docker rm snowman-multi
-docker run -d -p 4000:4000 --name snowman-multi mikemjharris/snowman-multi
+docker run --rm -d -p 4000:4000 --name snowman-multi mikemjharris/snowman-multi
 docker kill snowman
-docker rm snowman
-docker run -d -p 3000:8000 --name snowman mikemjharris/snowman 
+docker run --rm -d -p 3000:8000 --name snowman mikemjharris/snowman 
 docker kill animated-message
-docker rm animated-message
-docker run -d -p 8000:8080 --name animated-message mikemjharris/animated-message
+docker run --rm -d -p 8000:8080 --name animated-message mikemjharris/animated-message
 #docker kill mongodb
 #docker rm mongodb
 #docker run -d -p 27017:27017 -v /data/db:/data/db --name mongodb mongo
 docker kill blog
-docker rm blog
 #docker run -d -p 7000:8000 --link mongodb:mongodb --name blog mikemjharris/blog
-docker run -d -p 7000:8000  --name blog mikemjharris/blog
+docker run --rm -d -p 7000:8000  --name blog mikemjharris/blog
 docker kill star-wars-twitter
 docker run --rm -d -p 3001:3000 --name star-wars-twitter star-wars-new
 #docker kill b-reel
