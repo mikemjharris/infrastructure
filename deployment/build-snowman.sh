ac71ed8dcfea83151f20cf57453faddf9722f625
Mike Harris <hello@mikemjharris.com>
2021-11-17 18:46:59 +0000

    feat(mount folder for scores for snowman)

--- deployment/build-snowman.sh
+++ deployment/build-snowman.sh
@@ -4,5 +4,4 @@ git checkout master
 git pull
 docker build -t mikemjharris/snowman .
 docker kill snowman 
-docker rm snowman 
-docker run -d -p 3000:8000  --name snowman mikemjharris/snowman
+docker run --rm -d -v $(pwd)/data:/var/www/data -p 3000:8000  --name snowman mikemjharris/snowman
