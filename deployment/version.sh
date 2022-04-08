70886a6a4712153dc8354ffb3a0707a70f505514
Mike Harris <hello@mikemjharris.com>
2022-04-08 14:22:40 +0000

    feat(update paths for versioning personal website)

--- deployment/version.sh
+++ deployment/version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cat ../personal-website/version.json >> ../personal-website/releases.json
+cat ~/personal-website/version.json >> ~/personal-website/releases.json
 
-COMMIT_HASH=`cd ../personal-website && git rev-parse HEAD`
+COMMIT_HASH=`cd ~/personal-website && git rev-parse HEAD`
 
 DATE=`date +"%Y-%m-%d_%H-%M-%S"`
 echo $COMMIT_HASH
 
-cat ./version.template  | sed -E "s/COMMIT_HASH/$COMMIT_HASH/" | sed -E "s/BRANCH_NAME/master/" | sed -E "s/DEPLOY_DATE/$DATE/" > ../personal-website/version.json
+cat ~/deployment/version.template  | sed -E "s/COMMIT_HASH/$COMMIT_HASH/" | sed -E "s/BRANCH_NAME/master/" | sed -E "s/DEPLOY_DATE/$DATE/" > ~/personal-website/version.json
 
